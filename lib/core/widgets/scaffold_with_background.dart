c26532c9d906f1badb55568b7a299e9457c7013a
hosamshaban <97hosams@gmail.com>
2023-09-19 12:54:43 +0300

    remove unused

--- lib/core/widgets/scaffold_with_background.dart
+++ lib/core/widgets/scaffold_with_background.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_triad/config/constants.dart';
 import 'package:flutter_triad/core/resources/manager_colors.dart';
 import 'package:flutter_triad/core/resources/manager_sizes_util.dart';
 import 'package:flutter_triad/core/widgets/pop_cope_widget.dart';
-import 'package:flutter/material.dart';
-import '../resources/manager_assets.dart';
+
 import 'arrow_back_button.dart';
 
 Widget scaffoldWithImageBackground({
